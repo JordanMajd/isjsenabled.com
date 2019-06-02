9cacb97aad3dece74aa549a1d07232e244fa39c1
Jordan Majd <jordan@madtitanstudios.com>
2019-06-02 14:50:57 -0600

    Move live site to docs directory, minify sources

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# create dir if doesn't exist
+mkdir -p docs
+
+# minify
+# requires: npm install html-minifier -g
+html-minifier \
+    --collapse-whitespace \
+    --remove-comments \
+    --remove-optional-tags \
+    --remove-redundant-attributes \
+    --remove-script-type-attributes \
+    --remove-tag-whitespace \
+    --use-short-doctype \
+    --minify-css true \
+    --minify-js true \
+    index.html > ./docs/index.html
+
+# copy assets
+cp -R img docs/img
+cp favicon.ico ./docs/
+cp CNAME ./docs/
+cp robots.txt ./docs/
+cp sitemap.xml ./docs/
